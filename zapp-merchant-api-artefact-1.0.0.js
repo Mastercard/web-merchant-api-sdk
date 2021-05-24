54e2c72e19ce114ec6d37e29a04530f9ab9a3178
singh_mansi <mansi.singh@vocalink.com>
2021-05-24 16:00:09 +0100

    Added functionality for reading encoded and decoding JSON file. Updated Technical error coded.

--- zapp-merchant-api-artefact-1.0.0.js
+++ zapp-merchant-api-artefact-1.0.0.js
@@ -15,7 +15,7 @@ limitations under the License.
 */
 
 window.zappMerchantApi = window.zappMerchantApi || {};
-var cdnUrl = "https://zts3.co.uk/merchantApiCdnFiles/banks.json"; // CDN location to fetch the CFI logos
+const cdnUrl = "https://zts3.co.uk/merchantApiCdnFiles/sbibanks"; // CDN location to fetch the CFI logos
 
 zappMerchantApi.cfiDetail ={};	// Object containing CFI id, name and logo
 cfiMetaDataList = {};	// CFI metadata object
@@ -25,26 +25,35 @@ zappMerchantApi.getCfiDetails = new Promise(
     function (resolve, reject) {
     	
     	try{
-	        var bankJson = $.getJSON(cdnUrl)
-				.done(function(data){
-					zappMerchantApi.cfiCount = data.length;
+	    	 fetch(cdnUrl)
+	   		 .then(
+	   				 function(response) {
+	   					 if (response.status !== 200) {
+	   						postError("MC1001" , "Technical error occurred. "+ response.status);
+	   						return;
+	   				}
+	   				return response.text();
+	   		    })
+	   		    .then(function(text) {
+	   		    	let decodedRes = JSON.parse(atob(text));
+	   		        zappMerchantApi.cfiCount = decodedRes.length;
 					for(i=0 ; i<zappMerchantApi.cfiCount ; i++)
 					{
 						cfiJsonObj = {};
-						cfiJsonObj.cfiUniqueId = data[i].cfiUniqueId;
-						cfiJsonObj.cfiName = data[i].cfiName;
-						cfiJsonObj.cfiLogo = data[i].cfiLogo;
+						cfiJsonObj.cfiUniqueId = decodedRes[i].cfiUniqueId;
+						cfiJsonObj.cfiName = decodedRes[i].cfiName;
+						cfiJsonObj.cfiLogo = decodedRes[i].cfiLogo;
 						zappMerchantApi.cfiDetail[i] = cfiJsonObj;
-						cfiMetaDataList[data[i].cfiUniqueId] = data[i];
+						cfiMetaDataList[decodedRes[i].cfiUniqueId] = decodedRes[i];
 					}
 					resolve(true);
-				})
-				.fail(function(jqObj, errTyp, errTxt ) {
-						postError("S9998","An error has occurred while reading the JSON file. Error Type: " + errTyp + ". Error Text:"+ errTxt);
-				});
+	   		    })
+	   		  .catch(function(err) {
+	   			postError('MC1002' , " Technical error occurred. "+ err);
+	   		  });
     	}
     	catch(e){
-    		postError('S9999',"An unknown error has occured");
+    		postError('MC1003', " Technical error occurred.");
     	}
     }
 );
@@ -103,7 +112,7 @@ zappMerchantApi.invokeApp = function(cfiId,aptrId,productType){
 				postError(e.message,"No data present for CFI Id");
 				break;
 			default:
-				postError('S9997',"Unknown error has occured");
+				postError('MC1004',"Technical error occurred");
 				break;
 			}
 		
